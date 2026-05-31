4d52fbc4459aac1a01434bfbb77e30e1699c7803
Umer Nisar <umernisar053@gmail.com>
2026-06-01 00:59:16 +0500

    m

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,9 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
+{
+    if (env('APP_ENV') === 'production') {
+        \URL::forceScheme('https');
     }
+}
 }
